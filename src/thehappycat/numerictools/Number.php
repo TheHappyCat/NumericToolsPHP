e32474fcb4403b542dacd633355ce5a866af7a01
Jean Paul Ruiz <jpruiz114@gmail.com>
2017-11-24 19:57:05 -0500

    First commit

---
+++ src/thehappycat/numerictools/Number.php
@@ -0,0 +1,26 @@
+<?php namespace Company\NumericTools;
+
+/**
+ * Class Number
+ * @package TheHappyCat\NumericTools
+ */
+class Number
+{
+    /**
+     * @param Number $number
+     * @return $this
+     */
+    function add(Number $number)
+    {
+        return $this;
+    }
+
+    /**
+     * @param Number $number
+     * @return $this
+     */
+    function subtract(Number $number)
+    {
+        return $this;
+    }
+}
